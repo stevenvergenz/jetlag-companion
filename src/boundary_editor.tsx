d01e61c555c99f3b6530df3eeaa584e2a2965b43
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2025-06-24 18:22:50 -0700

    Point add

--- src/boundary_editor.tsx
+++ src/boundary_editor.tsx
@@ -1,52 +1,40 @@
-import { ReactNode, useContext, useState, useRef, useMemo } from 'react';
-import { LatLngTuple, LeafletEvent, LeafletEventHandlerFnMap, Marker as LeafletMarker } from 'leaflet';
+import { ReactNode, useContext, useState, useMemo } from 'react';
+import { LeafletEvent, LeafletEventHandlerFnMap, LineUtil, CRS } from 'leaflet';
 import { LayerGroup, Marker, Polyline, Tooltip } from 'react-leaflet';
 
 import { SharedContext } from './context';
 import { Position } from 'geojson';
 
+function contextAction(): string {
+    return !window.matchMedia('(pointer: fine') ? 'Long-press' : 'Right-click';
+}
+
 export default function BoundaryEditor(): ReactNode {
     const { boundaryPoints, setBoundaryPoints } = useContext(SharedContext);
-    const [ editPoints, setEditPoints ] = useState<LatLngTuple[]>(boundaryPoints.map(p => [p[1], p[0]]));
+    const [ editPoints, setEditPoints ] = useState(boundaryPoints);
 
-    const points = editPoints.slice(0, -1).map((p, i) => 
+    const points = editPoints.slice(0, -1).map((_, i) => 
         <BoundaryPointHandle
             key={ `boundary-point-${i}` }
-            pos={p}
-            dragPos={(newPos: LatLngTuple) => {
-                const newPoints = [...editPoints];
-                newPoints[i] = newPos;
-                if (i === 0) {
-                    newPoints[newPoints.length - 1] = newPos; // Ensure the last point matches the first
-                }
-                setEditPoints(newPoints);
-            }}
-            setPos={(newPos: LatLngTuple | null) => {
-                const newPoints = [...editPoints];
-                if (newPos) {
-                    newPoints[i] = newPos;
-                    if (i === 0) {
-                        newPoints[newPoints.length - 1] = newPos; // Ensure the last point matches the first
-                    }
-                } else if (newPoints.length > 4){
-                    newPoints.splice(i, 1);
-                    if (i === 0) {
-                        newPoints[newPoints.length - 1] = newPoints[0]; // Ensure the last point matches the first
-                    }
-                }
-                setEditPoints(newPoints);
-                setBoundaryPoints(newPoints.map(p => [p[1], p[0]] as Position));
+            i={i}
+            points={editPoints}
+            update={setEditPoints}
+            save={p => {
+                setEditPoints(p);
+                setBoundaryPoints(p);
             }}
         />
     );
 
-    const lines = editPoints.map((p, i, arr) =>
-        i < arr.length - 1 && <Polyline
+    const lines = editPoints.slice(0, -1).map((_, i) =>
+        <BoundaryEdgeHandle
             key={ `boundary-line-${i}` }
-            positions={[p, arr[i + 1]]}
-            color='black'
-            weight={2}
-            interactive={false}
+            i={i}
+            points={editPoints}
+            save={p => {
+                setEditPoints(p);
+                setBoundaryPoints(p);
+            }}
         />
     );
 
@@ -57,45 +45,80 @@ export default function BoundaryEditor(): ReactNode {
 }
 
 type BoundaryPointHandleProps = {
-    pos: LatLngTuple,
-    dragPos: (p: LatLngTuple) => void,
-    setPos: (p: LatLngTuple | null) => void,
+    i: number,
+    points: Position[],
+    update: (p: Position[]) => void,
+    save: (p: Position[]) => void,
 };
 
-function BoundaryPointHandle({ pos, dragPos, setPos }: BoundaryPointHandleProps): ReactNode {
-    const markerRef = useRef<LeafletMarker | null>(null);
-
+function BoundaryPointHandle({ i, points, update, save }: BoundaryPointHandleProps): ReactNode {
     const handlers = useMemo<LeafletEventHandlerFnMap>(() => ({
         drag: ({ target }: LeafletEvent) => {
-            if (target) {
-                const latlng = target.getLatLng();
-                dragPos([latlng.lat, latlng.lng]);
-            }
+            if (!target) { return; }
+            const latlng = target.getLatLng();
+            const newPoints = [...points];
+            newPoints[i] = [latlng.lng, latlng.lat] satisfies Position;
+            newPoints[newPoints.length - 1] = newPoints[0]; // Ensure the last point matches the first
+            update(newPoints);
         },
         dragend: ({ target }: LeafletEvent) => {
-            if (target) {
-                const latlng = target.getLatLng();
-                setPos([latlng.lat, latlng.lng]);
-            }
+            if (!target) { return; }
+            const latlng = target.getLatLng();
+            const newPoints = [...points];
+            newPoints[i] = [latlng.lng, latlng.lat] satisfies Position;
+            newPoints[newPoints.length - 1] = newPoints[0]; // Ensure the last point matches the first
+            save(newPoints);
         },
         contextmenu: ({ target }: LeafletEvent) => {
-            if (target) {
-                setPos(null);
-            }
+            if (!target || points.length <= 4) { return; }
+            const newPoints = [...points];
+            newPoints.splice(i, 1);
+            newPoints[newPoints.length - 1] = newPoints[0]; // Ensure the last point matches the first
+            save(newPoints);
         },
-    }), [dragPos, setPos]);
+    }), [i, points, update, save]);
 
-    const deleteActionStr = !window.matchMedia('(pointer: fine') ? 'long-press' : 'right-click';
-    
     return <Marker
-        position={pos}
+        position={[points[i][1], points[i][0]]}
         interactive={true}
         draggable={true}
-        ref={markerRef}
         eventHandlers={handlers}
     >
         <Tooltip>
-            {`Drag to move, ${deleteActionStr} to delete`}
+            {`Drag to move, ${contextAction()} to delete`}
         </Tooltip>
     </Marker>;
+}
+
+type BoundaryEdgeHandleProps = {
+    i: number,
+    points: Position[],
+    save: (points: Position[]) => void,
+};
+
+function BoundaryEdgeHandle({ i, points, save }: BoundaryEdgeHandleProps): ReactNode {
+    const handlers = useMemo<LeafletEventHandlerFnMap>(() => ({
+        contextmenu: ({ target }: LeafletEvent) => {
+            if (!target) { return; }
+            const p1 = points[i];
+            const p2 = points[i + 1];
+            const pMid = LineUtil.polylineCenter([[p1[1], p1[0]], [p2[1], p2[0]]], CRS.EPSG3857);
+
+            const newPoints = [...points];
+            newPoints.splice(i + 1, 0, [pMid.lng, pMid.lat] satisfies Position);
+            save(newPoints);
+        },
+    }), [i, points, save]);
+
+    return <Polyline
+        positions={[[points[i][1], points[i][0]], [points[i + 1][1], points[i + 1][0]]]}
+        color='black'
+        weight={2}
+        interactive={true}
+        eventHandlers={handlers}
+    >
+        <Tooltip>
+            {`${contextAction()} to split`}
+        </Tooltip>
+    </Polyline>;
 }
